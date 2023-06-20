989a58c0ff252fddd2984f7ce750cebe9156f867
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-06-21 00:12:47 +0900

    [Gold IV] Title: 플로이드, Time: 228 ms, Memory: 116064 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,9 +1,8 @@
 import sys
 
 INF = int(1e9)
-
-n = int(input())    # city
-m = int(input())    # bus
+n = int(sys.stdin.readline().rstrip())
+m = int(sys.stdin.readline().rstrip())
 
 graph = [[INF] * (n+1) for _ in range(n+1)]
 
@@ -14,20 +13,20 @@ for a in range(1, n+1):
 
 for _ in range(m):
     a,b,c = map(int, sys.stdin.readline().split())
-    graph[a][b] = min(graph[a][b], c)
+    if graph[a][b]>c:
+        graph[a][b]=c
 
-
-
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] >= INF:
+            graph[a][b] = 0
 
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if graph[a][b] == INF:
-            print('0', end=' ')
-        else:
-            print(graph[a][b],end=' ')
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        print(graph[a][b], end=' ')
     print()
