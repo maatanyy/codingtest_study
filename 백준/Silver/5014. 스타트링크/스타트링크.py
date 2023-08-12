778e74da3f485260817ed9cdac753dc54a6b89ef
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-08-12 15:02:55 +0900

    [Silver I] Title: 스타트링크, Time: 508 ms, Memory: 41516 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,47 +1,35 @@
+# 총 F층, 스타트 링크 G층, 강호가 있는 곳 S층
+# U버튼 은 위로 U, D 버튼은 아래로 D층
+# 갈 수 없다면 "use the stairs"
+
 import sys
+sys.setrecursionlimit(100000000)
 from collections import deque
+
 F, S, G, U, D = map(int, input().split())
 
-# 총 F층 건물
-# 스타트링크가 있는 곳 G
-# 강호가 지금 있는 곳 S
-# U는 위로 U
-# D는 아래로 D
-
-change = [U, -D]
+changes = [U, -D]
 
 def bfs(S):
-    visited = [False] * (F + 1)
+    visited = [False] * (F+1)
     visited[S] = 1
     q = deque()
     q.append((S, 0))
 
     while q:
-        loc, count = q.popleft()
+        location, count = q.popleft()
 
-        if loc == G:
+        if location == G:
             return count
 
         for i in range(2):
-            newloc = loc+change[i]
-            if 1<=newloc<=F and visited[newloc]==False:
-                q.append((newloc,count+1))
-                visited[newloc] = True
+            newlocation = location+changes[i]
 
+            if 1<=newlocation<=F and visited[newlocation]==False:
+                q.append((newlocation,count+1))
+                visited[newlocation] = True
 
     return "use the stairs"
 
+
 print(bfs(S))
-
-
-
-
-
-
-
-
-
-
-
-
-
