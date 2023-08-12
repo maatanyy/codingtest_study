a3c957565dca35c57cdf08561e3512d0c6d520ad
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-08-12 14:14:52 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 32484 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,57 +1,52 @@
 import sys
-from collections import deque
 
-m,n,k = map(int,input().split())
+sys.setrecursionlimit(100000000)
+m, n, k = map(int, input().split())
+# m은 세로 n은 가로
 
 graph = [[0 for _ in range(n)] for _ in range(m)]
 
+#print(graph)
+
 for _ in range(k):
-    a,b,c,d = map(int,input().split())
-    # 0~4, 2~4
-    for i in range(b,d):  #2,4
-        for j in range(a,c):  #0,4
-            graph[i][j] = 1
+    x1, y1, x2, y2 = map(int, input().split())
 
+    for i in range(x1,x2):
+        for j in range(y1,y2):
+            graph[j][i] = 1
 
-nx = [1,-1,0,0]
-ny = [0,0,1,-1]
+#print(graph)
 
-def bfs(x,y):
-    count = 1
-    queue = deque()
-    queue.append((x,y))
-    graph[x][y] = 1
+def dfs(x,y):
 
-    while queue:
-        a,b = queue.popleft()
+    if x<0 or y<0 or x>=m or y>=n:
+        return False
 
-        for i in range(4):
-            newx = a+nx[i]
-            newy = b+ny[i]
+    if 0<=x<m and 0<=y<n and graph[x][y]==0:
+        graph[x][y] =1
+        global count
+        count+=1
 
-            if 0<=newx<m and 0<=newy<n and graph[newx][newy]==0:
-                graph[newx][newy] = 1
-                queue.append((newx,newy))
-                count+=1
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        return True
 
-    return count
+    return False
 
-ans = []
-count2 = 0
+count = 0
+
+vals = []
+
 for i in range(m):
     for j in range(n):
-        if graph[i][j] == 0:
-            ans.append(bfs(i,j))
-            count2+=1
+        if dfs(i,j)==True:
+            vals.append(count)
+            count = 0
 
-ans.sort()
-
-print(count2)
-for i in ans:
+vals.sort()
+print(len(vals))
+for i in vals:
     print(i, end=' ')
-
-
-
-
-
 
