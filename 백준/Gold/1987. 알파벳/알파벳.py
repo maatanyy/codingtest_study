a480ee2360b4b3f4b85b1dbaac89bd93f38b3adb
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-08-12 16:09:45 +0900

    [Gold IV] Title: 알파벳, Time: 1460 ms, Memory: 52756 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -5,25 +5,28 @@ n, m = map(int, input().split())
 graph = [list(input()) for _ in range(n)]
 
 answer = 0
-check = [0]*(26)
+check = [0] * (26)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs(x, y, count):
+answer = 1
+
+def bfs(x, y):
 
     global answer
-    answer = max(answer, count)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and check[ord(graph[nx][ny])-65]==0:
-            check[ord(graph[nx][ny])-65]=1
-            dfs(nx, ny, count+1)            #돌리는데
-            check[ord(graph[nx][ny])-65]=0  #remove 왜냐하면 최대가 아닐경우 새로운 길로 갈때 알파벳 빼줘야함
+    q = set([(x,y,graph[x][y])])
 
+    while q:
+        x, y, alpha = q.pop()
+        for i in range(4):
+            newX = x+dx[i]
+            newY = y+dy[i]
 
-check[ord(graph[0][0])-65]=1
-dfs(0, 0, 1)
+            if 0<=newX<n and 0<=newY<m and graph[newX][newY] not in alpha:
+                q.add((newX,newY, alpha+graph[newX][newY]))
+                answer = max(answer, len(alpha)+1)
 
+bfs(0,0)
 print(answer)
+
