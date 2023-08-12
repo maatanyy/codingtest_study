c5d0f580b4bd59e6dac483ae36d3ad169f6a0a33
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-08-12 16:03:42 +0900

    [Gold IV] Title: 알파벳, Time: 5120 ms, Memory: 153956 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,18 +1,7 @@
-########################
-# alphabets.add(graph[nx][ny])
-# dfs(nx, ny, count+1)
-# alphabets.remove(graph[nx][ny])
-# https://sorryhyeon.tistory.com/34 참고
-# dfs에서 백트랙킹
-# 찾아보니 시간초과가 너무 많이 나서 정답률이 낮음, 문제가 좀 별로인감
-
 import sys
 from collections import deque
 
-
 n, m = map(int, input().split())
-
-
 graph = [list(input()) for _ in range(n)]
 
 answer = 0
@@ -31,7 +20,7 @@ def dfs(x, y, count):
         if 0 <= nx < n and 0 <= ny < m and check[ord(graph[nx][ny])-65]==0:
             check[ord(graph[nx][ny])-65]=1
             dfs(nx, ny, count+1)            #돌리는데
-            check[ord(graph[nx][ny])-65]=0 #remove 왜냐하면 최대가 아닐경우 새로운 길로 갈때 알파벳 빼줘야함
+            check[ord(graph[nx][ny])-65]=0  #remove 왜냐하면 최대가 아닐경우 새로운 길로 갈때 알파벳 빼줘야함
 
 
 check[ord(graph[0][0])-65]=1
