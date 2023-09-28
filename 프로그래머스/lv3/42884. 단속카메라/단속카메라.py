d659b844700f3d46882c46c59c57638ee232b01a
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-09-28 23:03:52 +0900

    [level 3] Title: 단속카메라, Time: 0.97 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/lv3/42884. 단속카메라/단속카메라.py
@@ -1,15 +1,16 @@
 def solution(routes):
     answer = 0
-    routes.sort(key=lambda x: x[1])
-    print(routes)
-    check = -30001
-
-    for route in routes:
-        if check < route[0]:
-            answer += 1
-            check = route[1]
-
+    
+    routes.sort(key = lambda x:x[1])
+    
+    camera = -30001
+    
+    for i in routes:
+        a, b = i[0], i[1]
+        
+        if camera < i[0]:
+            camera = i[1]
+            answer+=1
+            
+    
     return answer
-
-routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
-print(solution(routes))
