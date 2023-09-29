a9589db28aef3f4943758ef1f5a2c35250a6deff
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-09-29 15:30:12 +0900

    [level 2] Title: 더 맵게, Time: 1832.14 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,37 +1,22 @@
 import heapq
-
-
 def solution(scoville, K):
     answer = 0
     q = []
+    
     for i in scoville:
-        heapq.heappush(q, i)
-
-    while q:
-
-        if len(q) == 2:
-            val1 = heapq.heappop(q)
-            val2 = heapq.heappop(q)
+        heapq.heappush(q,i)
+        
+    while True:
             
-            if val1>=K:
-                return answer
-            
-            elif val1 + 2 * val2 < K:
-                return -1
-
-            elif val1 + 2 * val2 >= K:
-                return answer+1
-
+        a = heapq.heappop(q)
+        
+        if a>=K:
+            break
         else:
-            val1 = heapq.heappop(q)
-            val2 = heapq.heappop(q)
-
-            if val1 >= K:
-                return answer
-
-            else:
-                answer += 1
-                val3 = val1 + 2 * val2
-                heapq.heappush(q, val3)
-
+            if len(q)==0:
+                return -1
+            b = heapq.heappop(q)
+            answer+=1
+            heapq.heappush(q,a+2*b)
+        
     return answer
