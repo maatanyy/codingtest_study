37e663d67abf2215da556161d18254c737452d8a
Min Sung <76435279+maatanyy@users.noreply.github.com>
2023-09-30 16:34:40 +0900

    [level 2] Title: 주식가격, Time: 73.65 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,14 +1,14 @@
 def solution(prices):
-    answer = [0] * len(prices)
-
+    answer = []
+    
     for i in range(len(prices)):
         count = 0
-        for j in range(i,len(prices)-1):
-            if prices[i]<=prices[j]:
+        for j in range(i+1,len(prices)):
+            if prices[i] <= prices[j]:
                 count+=1
             else:
-                answer[i] =count
+                count+=1
                 break
-            answer[i] =count
-
+        answer.append(count)
+            
     return answer
