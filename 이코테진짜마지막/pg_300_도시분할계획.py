9c6181cb402ad92f8b011af226983b494459def3
minsung <sbr04119@naver.com>
2024-01-17 17:36:11 +0900

    2024-1-17 이코테 복습10

---
+++ 이코테진짜마지막/pg_300_도시분할계획.py
@@ -0,0 +1,41 @@
+n, m = map(int,input().split())
+
+graph = [[] for _ in range(n+1)]
+
+parent = [0] * (n+1)
+edges = []
+result = 0
+
+for i in range(1,n+1):
+    parent[i] = i
+
+def find_parent(parent,x):
+    if parent[x]!=x:
+        return find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] =b 
+        
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    edges.append((c,a,b)) 
+    
+edges.sort()
+
+for edge in edges:
+    c, a, b = edge
+    if find_parent(parent,a) !=find_parent(parent,b):
+        union_parent(parent,a,b)
+        
+        result+=c
+        last = c
+
+print(result-last)
+    
