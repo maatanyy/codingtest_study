fff7cc3d9a23ca01ce590ce5cd73f5acd5afc4d3
Min Sung <76435279+maatanyy@users.noreply.github.com>
2024-01-30 00:25:03 +0900

    [Silver IV] Title: 걷기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.py
+++ 백준/Silver/1459. 걷기/걷기.py
@@ -1,31 +1,26 @@
-# 처음에 대각선  ㅅ 형태를 생각 못했음
-# 그 다음은 이제 어떻게 최소를 할까 고민했는데 나는 한 좌표가 0 일경우 직선을 고려했는데
-# x,y 최소와 최대 중 하나를 먼저 다 간 상태로 나머지 골하는 방식을 생각을 못했음
-# https://stopthebackspace.tistory.com/m/18 참고했음
-import sys
+x, y, w, s = map(int,input().split())
+# w 한블럭, s 대각선
+ans = 0
 
-x, y, cost1, cost2 = map(int, sys.stdin.readline().split())
-# x, y, 걸어서, 대각선
 
-if cost1 * 2 <=cost2:
-    answer = x*cost1 + y*cost1
+if 2 * w >=s:
 
-else:
-    temp = min(x,y)
-    temp2 = max(x,y)
-    temp3 = temp2-temp
+    ans = ans + min(x,y) *s
 
-    answer = temp * cost2
+    temp = max(x,y) - min(x,y)
 
-    if cost1>cost2:
-        if temp3%2==0:
-            answer+=temp3*cost2
+    if temp%2 == 0:
 
-        else:
-            answer+=(temp3-1)*cost2+cost1
+        tempcost = min(2*s, 2*w)
+        ans = ans + tempcost * (temp//2)
 
     else:
-        answer+=temp3*cost1
+        tempcost = min(2*s, 2*w)
+        ans = ans + tempcost * (temp//2)
+        ans = ans + w
 
-print(answer)
+else:
 
+    ans += (x+y)*w
+
+print(ans)
