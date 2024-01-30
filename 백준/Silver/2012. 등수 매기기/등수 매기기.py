85a37afc8bce4073875c1d0f64c5a69906056732
Min Sung <76435279+maatanyy@users.noreply.github.com>
2024-01-30 11:52:56 +0900

    [Silver III] Title: 등수 매기기, Time: 324 ms, Memory: 144604 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -1,26 +1,18 @@
 import sys
 num = int(input())
 
-# 예상등수
-tempans = []
+temp = []
 
-# 실제등수
-ans = []
+for _ in range(num):
+    temp.append(int(sys.stdin.readline()))
 
-for i in range(num):
-    ans.append(i+1)
+temp.sort()
 
-for i in range(num):
-    x = int(sys.stdin.readline())
-    tempans.append(x)
+ans = [i+1 for i in range(num)]
 
-tempans.sort()
+answer = 0
 
-sum = 0
+for i in range(len(temp)):
+    answer += abs(temp[i]-ans[i])
 
-for i in range(num):
-    if ans[i]!=tempans[i]:
-        sum += abs(ans[i]-tempans[i])
-
-print(sum)
-
+print(answer)
