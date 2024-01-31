35bada70c861615d5a78971c9fb62ee9d3b808c2
Min Sung <76435279+maatanyy@users.noreply.github.com>
2024-01-31 19:03:09 +0900

    [Silver II] Title: 주식, Time: 1424 ms, Memory: 322140 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,24 +1,22 @@
-# 앞부터 접근하여서 시간 초과남...
-# 결국 풀이가 안떠올라서 찾아봤더니
-# 뒤에서 부터 접근하며 최대값을 변경하는식으로 시간초과를 해결하더라
-# 다음에 나오면 이런 사고가 가능하면 좋을듯
-
 import sys
-from itertools import combinations
+testcase = int(input())
 
-num = int(input())
+for _ in range(testcase):
 
-for _ in range(num):
+    answer = 0
+
     day = int(input())
-    tempval = list(map(int, sys.stdin.readline().split()))
+    temp = list(map(int,sys.stdin.readline().split()))
+    
+    first = temp[-1]
 
-    ans = 0
-    now = tempval[-1]
-    for i in range(day - 1, -1, -1):
-        if tempval[i] > now:
-            now = tempval[i]
+    for k in range(day-1,-1,-1):
+        
+        if temp[k] > first:
+            first = temp[k]
+        
         else:
-            ans += now - tempval[i]
-    print(ans)
+            answer+= (first - temp[k])
 
-
+        
+    print(answer)
