917fac6fb7b4151105b51f2c7e9eb6100f5b68f5
Min Sung <76435279+maatanyy@users.noreply.github.com>
2024-02-05 12:45:01 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114260 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -5,32 +5,38 @@ n, m = map(int,input().split())
 graph = []
 
 for _ in range(n):
-    graph.append(list(map(int,input())))
+    graph.append(list(map(int, input())))
 
-visited = [[False]* m for _ in range(n)]
+visited = [[False]*(m) for _ in range(n)]
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
 
-def bfs(x, y):
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+
+def bfs(x,y):
+
     queue = deque([(x, y)])
     visited[x][y] = True
-    
-    
+
     while queue:
-        x, y = queue.popleft()
-        
+
+        x,y = queue.popleft()
+
         for i in range(4):
-            newX = x+dx[i]
-            newY = y+dy[i]
-            
-            if 0<=newX <n and 0<=newY <m  and visited[newX][newY]==False and graph[newX][newY]==1:
-                graph[newX][newY]= graph[x][y]+1
-                visited[newX][newY] = True
-                queue.append((newX,newY))
-                
-    return graph[n-1][m-1]
-        
+            newx = x+dx[i]
+            newy = y+dy[i]
+
+            if 0<=newx < n and 0<=newy <m and visited[newx][newy] == False and graph[newx][newy]==1:
+
+                graph[newx][newy] = graph[x][y]+1
+                visited[newx][newy] = True
+
+                queue.append((newx,newy))
 
 
-print(bfs(0, 0))
+
+
+bfs(0,0)
+
+print(graph[n-1][m-1])
