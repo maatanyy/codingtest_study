c5e4248752eb91baa97c31f922cc1a888d09b895
Min Sung <76435279+maatanyy@users.noreply.github.com>
2024-02-06 23:27:50 +0900

    [Gold IV] Title: 최단경로, Time: 476 ms, Memory: 135272 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,49 +1,48 @@
-import heapq
 import sys
-n, m = map(int, sys.stdin.readline().split())
-
-start = int(input())
+import heapq
 
 INF = int(1e9)
 
-graph = [ []*(n+1) for _ in range(n+1)]
+v, e = map(int,input().split())
+start = int(input())
 
-distance = [INF] * (n+1)
+distance = [INF] * (v+1)
 
-for _ in range(m):
-    a,b,c = map(int, sys.stdin.readline().split())
+graph = [[]*(v+1) for _ in range(v+1)]
+
+for _ in range(e):
+    a,b,c = map(int,sys.stdin.readline().split())
+
     graph[a].append((b,c))
+
+
+
 
 def dij(start):
 
-    q = []
-    heapq.heappush(q,(0, start))  #거리, 노드
     distance[start] = 0
+    q = []
+
+    heapq.heappush(q,(0, start))
 
     while q:
-        dis, nod = heapq.heappop(q)
+        dist, now = heapq.heappop(q)
 
-        if distance[nod] < dis:
+        if distance[now] < dist:
             continue
 
-        for i in graph[nod]:
-            cost = dis + i[1]
+        for i in graph[now]:
+
+            cost = dist + i[1]
 
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
-                heapq.heappush(q,(cost,i[0]))
+                heapq.heappush(q,(cost, i[0]))
 
 dij(start)
 
-for i in range(len(distance)-1):
-    if distance[i+1]== INF:
+for i in range(1,v+1):
+    if distance[i]==INF:
         print('INF')
     else:
-        print(distance[i+1])
-
-
-
-
-
-
-
+        print(distance[i])
